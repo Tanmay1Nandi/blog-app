474ce2ff9da93b72f7f87b712bdafdde14e1b2c5
Tanmay1Nandi <118953751+Tanmay1Nandi@users.noreply.github.com>
2025-02-19 18:21:18 +0530

    Created User Model

--- api/index.js
+++ api/index.js
@@ -5,12 +5,6 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8005;
 
 
-//connect to Database
-// const connectToDb = require("./connection")
-// connectToDb("mongodb://127.0.0.1:27017/BlogApp")
-// .then(()=> console.log("Connected to Db!"))
-// .catch((err) => console.log("Couldn't connect due to" + err));
-
 mongoose.connect(process.env.URL)
 .then(()=> console.log("Connected to Db"))
 .catch((err) => {
