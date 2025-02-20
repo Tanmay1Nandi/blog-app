572eef4d91448a6485db9622f9341f13466bebf4
Tanmay1Nandi <118953751+Tanmay1Nandi@users.noreply.github.com>
2025-02-20 07:16:56 +0530

    created signup api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8005;
 
+app.use(express.json());
 
 mongoose.connect(process.env.URL)
 .then(()=> console.log("Connected to Db"))
@@ -14,5 +15,7 @@ mongoose.connect(process.env.URL)
 app.listen(PORT, ()=> console.log(`Server is running at PORT ${PORT}!`));
 
 const userRoutes = require("./routes/user.route")
+const authRoutes = require("./routes/auth.route")
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
