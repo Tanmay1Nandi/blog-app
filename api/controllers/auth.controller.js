530dc8e6d755294c1057c5a6612aca28e039f573
Tanmay1Nandi <118953751+Tanmay1Nandi@users.noreply.github.com>
2025-02-20 16:21:13 +0530

    Create Signin Api Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
-const bcrypt = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken")
 
 async function signup(req, res, next){
     const { username, email, password } = req.body;
@@ -9,7 +10,7 @@ async function signup(req, res, next){
         next(errorHandler(400, 'All fields are requried'));
     }
 
-    const hashedPassword = bcrypt.hashSync(password, 10);
+    const hashedPassword = bcryptjs.hashSync(password, 10);
 
     const newUser = new User({
         username,
@@ -26,4 +27,37 @@ async function signup(req, res, next){
     }
 }
 
-module.exports = signup;
+
+async function signin(req, res, next){
+    const{ email, password } = req.body;
+    if(!email || !password || email === "" || password === ""){
+        next(errorHandler(400, "All fields are required"));
+    }
+
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, "Password or email not correct"));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword){
+            return next(errorHandler(404, "Password or email not correct"));
+        }
+        const token = jwt.sign(
+            {id: validUser._id}, process.env.JWT_SECRET, {expiresIn: "10d"}
+        )
+        const {password: pass, ...rest} = validUser._doc;
+
+        res.status(200).cookie('access_token', token,{
+            httpOnly: true
+        }).json(rest);
+    }catch(err){
+        next(err);
+    }
+}
+
+
+module.exports = {
+    signin,
+    signup
+};
